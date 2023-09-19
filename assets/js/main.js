dd1c3e8d7a26d3664bfcadb4f6c573d83609713f
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-19 17:47:20 +0600

    sorting dropdown clickable and refresh button also clickable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,60 @@
 const handleFromButton = () => {
     const fromList = document.getElementById('fromList');
     const fromListStyle = fromList.style.display;
-    if(fromListStyle != 'block'){
-        fromList.classList.remove('hidden')
-        console.log('block hoyca');
+    if(fromListStyle != 'flex'){
+        fromList.style.display = 'flex';
     }
     else{
-        fromListStyle == 'flex'
+        fromList.style.display = 'none';
     }
+}
+
+const handleFromList = (value) => {
+    const fromValue = document.getElementById('from');
+    const fromList = document.getElementById('fromList');
+    fromValue.innerText = value;
+    fromList.style.display = 'none';
+}
+const handleToButton = () => {
+    const toList = document.getElementById('toList');
+    const toListStyle = toList.style.display;
+    if(toListStyle != 'flex'){
+        toList.style.display = 'flex';
+    }
+    else{
+        toList.style.display = 'none';
+    }
+}
+
+const handleToList = (value) => {
+    const toValue = document.getElementById('to');
+    const toList = document.getElementById('toList');
+    toValue.innerText = value;
+    toList.style.display = 'none';
+}
+const handleTypeButton = () => {
+    const typeList = document.getElementById('typeList');
+    const typeListStyle = typeList.style.display;
+    if(typeListStyle != 'flex'){
+        typeList.style.display = 'flex';
+    }
+    else{
+        typeList.style.display = 'none';
+    }
+}
+
+const handleTypeList = (value) => {
+    const typeValue = document.getElementById('type');
+    const typeList = document.getElementById('typeList');
+    typeValue.innerText = value;
+    typeList.style.display = 'none';
+}
+
+const refreshSorting = () => {
+    const fromValue = document.getElementById('from');
+    const toValue = document.getElementById('to');
+    const typeValue = document.getElementById('type');
+    fromValue.innerText = 'From:';
+    toValue.innerText = 'To:';
+    typeValue.innerText = 'Type:';
 }
