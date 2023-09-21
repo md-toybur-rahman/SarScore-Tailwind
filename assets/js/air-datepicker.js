f514328a8692784472578532c55f059059e50145
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-21 11:25:29 +0600

    simple commit

--- assets/js/air-datepicker.js
+++
@@ -1,9 +0,0 @@
-import AirDatepicker from "air-datepicker";
-import en from "air-datepicker/locale/en";
-import "air-datepicker/air-datepicker.css";
-
-let options = {
-  locale: en
-};
-
-let dp = new AirDatepicker("#dp", options);
