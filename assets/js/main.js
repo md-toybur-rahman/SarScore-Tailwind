347442a46f37e0f85a3a7426aeec9594c7e60056
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-24 15:28:37 +0600

    working with SVG Map

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,3 +125,32 @@ for (let i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+
+
+new svgMap({
+    targetElementID: 'svgMap',
+    data: {
+        data: {
+            gdp: {
+                name: 'GDP per capita',
+                format: '{0} USD',
+                thousandSeparator: ',',
+                thresholdMax: 50000,
+                thresholdMin: 1000
+            },
+            change: {
+                name: 'Change to year before',
+                format: '{0} %'
+            }
+        },
+        applyData: 'gdp',
+        values: {
+            AF: { gdp: 587, change: 4.73 },
+            AL: { gdp: 4583, change: 11.09 },
+            DZ: { gdp: 4293, change: 10.01 },
+            AF_NA: { gdp: 4293, change: 10.01 }
+            // ...
+        }
+    }
+});
