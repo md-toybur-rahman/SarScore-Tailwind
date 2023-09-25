1b3846947f97b132b05a84d8a2073c1d5a009871
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-25 13:10:50 +0600

    make offline for svg map, font awesome, google font, datepicker and chart.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
 
+const dropdownButtonHandler = (ListId) => {
+    const List = document.getElementById(ListId);
+    const ListStyle = List.style.display;
+    if (ListStyle != 'flex') {
+        List.style.display = 'flex';
+    }
+    else {
+        List.style.display = 'none';
+    }
+}
+
+const dropdownListHandler = (Id, ListId, value) => {
+    const Value = document.getElementById(Id);
+    const List = document.getElementById(ListId);
+    Value.innerText = value;
+    List.style.display = 'none';
+}
+const dropdownInputListHandler = (Id, ListId, value) => {
+    const Value = document.getElementById(Id);
+    const List = document.getElementById(ListId);
+    Value.value = value;
+    List.style.display = 'none';
+}
+
+
+
 
 
 const handlePriorityButton = () => {
