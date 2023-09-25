6e96d2312e0849e33351090f57ec61c5eac3fb89
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-25 17:35:44 +0600

    complete Accuracy panel in Accessibility Section

--- assets/js/libs/chart_js/chart.js
+++ assets/js/libs/chart_js/chart.js
@@ -104,3 +104,21 @@ new Chart(accuracyChart, {
         }
     }
 });
+
+const verificationChart = document.getElementById('verification-chart');
+
+new Chart(verificationChart, {
+    type: 'doughnut',
+    data: {
+        datasets: [{
+            label: 'My First Dataset',
+            data: [30, 20, 50],
+            backgroundColor: [
+                '#28C66F',
+                '#6259CE',
+                '#63E2F1'
+            ],
+            hoverOffset: 4
+        }]
+    }
+})
