d79b85217a0f017f3a85ab77b40889eb9ec6829f
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-25 16:41:36 +0600

    working in data accuracy part

--- assets/js/libs/svg_map/svg_map_customize.js
+++ assets/js/libs/svg_map/svg_map_customize.js
@@ -23,3 +23,29 @@ new svgMap({
         }
     }
 });
+
+new svgMap({
+    targetElementID: 'heatSvgMap',
+    data: {
+        data: {
+            gdp: {
+                name: 'GDP per capita',
+                format: '{0} USD',
+                thousandSeparator: ',',
+                thresholdMax: 50000,
+                thresholdMin: 1000
+            },
+            change: {
+                name: 'Change to year before',
+                format: '{0} %'
+            }
+        },
+        applyData: 'gdp',
+        values: {
+            US: { gdp: 587, change: 4.73, color: '#36D399' },
+            AU: { gdp: 4583, change: 11.09, color: '#3ABFF8' },
+            BD: { gdp: 4293, change: 10.01, color: '#F000B8' },
+            // ...
+        }
+    }
+});
