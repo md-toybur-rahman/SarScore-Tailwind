74c35d5d489578fd8fc09987ace6cd0698a676ac
md-toybur-rahman <toyburrahman735@gmail.com>
2023-09-27 10:13:19 +0600

    FIXED SLIMSELECT ID ISSUE

--- assets/js/libs/slim_select/slimselect_customize.js
+++ assets/js/libs/slim_select/slimselect_customize.js
@@ -1,3 +1,24 @@
 new SlimSelect({
     select: '#selectElement'
 })
+new SlimSelect({
+    select: '#selectElement1'
+})
+new SlimSelect({
+    select: '#selectElement2'
+})
+new SlimSelect({
+    select: '#selectElement3'
+})
+new SlimSelect({
+    select: '#selectElement4'
+})
+new SlimSelect({
+    select: '#selectElement5'
+})
+new SlimSelect({
+    select: '#selectElement6'
+})
+new SlimSelect({
+    select: '#selectElement7'
+})
