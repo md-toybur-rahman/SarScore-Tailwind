6666826c3a8ebf26929dddd627acdb02fcb30955
md-toybur-rahman <toyburrahman735@gmail.com>
2023-10-12 11:39:39 +0600

    finish without revise

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,30 @@
-function optionActivate (id) {
+const ticketList = document.getElementById('ticketList');
+const singleTicket = ticketList.getElementsByTagName('tr');
+for (let i = 0; i < singleTicket.length; i++) {
+    const element = singleTicket[i];
+
+    element.addEventListener('click', function () {
+        const ticketContainer = document.getElementById('supportTicketContainer');
+        const singleTicket = document.getElementById('singlePage');
+        ticketContainer.style.display = 'none';
+        singleTicket.style.display = 'block';
+        window.scroll({
+            top: 0,
+            left: 0,
+            behavior: "smooth"
+        })
+    })
+}
+
+function handleBackButton() {
+    const ticketContainer = document.getElementById('supportTicketContainer');
+    const singleTicket = document.getElementById('singlePage');
+    ticketContainer.style.display = 'block';
+    singleTicket.style.display = 'none';
+}
+
+
+function optionActivate(id) {
     const buttonContainer = document.getElementById('buttonTest');
     const selectedButton = document.getElementById(id)
     const allButtons = buttonContainer.getElementsByTagName('button');
@@ -10,6 +36,21 @@ function optionActivate (id) {
 
 }
 
+
+function handleTicketMenu() {
+    const menuContainer = document.getElementById('ticketMenu');
+    const ticketArrow = document.getElementById('ticketArrow');
+    if (menuContainer.classList.contains('h-[150px]')) {
+        menuContainer.classList.remove('h-[150px]');
+        menuContainer.style.height = 'auto';
+        ticketArrow.style.rotate = '180deg';
+    }
+    else {
+        menuContainer.classList.add('h-[150px]');
+        menuContainer.style.height = '150px';
+        ticketArrow.style.rotate = '0deg'
+    }
+}
 
 // Dropdown function
 const dropdownButtonHandler = (ListId) => {
